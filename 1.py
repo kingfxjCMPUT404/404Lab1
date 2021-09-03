f7be44f3cba3e61734fb030a3e87fffc99bf27c3
King Fu <xinjian@cs.ualberta.ca>
2021-09-03 15:53:20 -0600

    Update python file

--- 1.py
+++ 1.py
@@ -1,7 +1,22 @@
+# 1. Version of requests
+# https://www.codegrepper.com/code-examples/python/print+version+of+requests+python
+# by Bored Coder
+# 2. Resquests to a url page
+# https://stackoverflow.com/a/15869929
+# By Alex K
+
 import requests
 
 print('Requests version:', requests.__version__, '\n')
 
 google = requests.get("https://www.google.com/")
 
-print(google.text)
+print(google.text, '\n')
+
+git = requests.get("https://raw.githubusercontent.com/kingfxj/404Lab1/master/1.py")
+
+f = open('1.py', 'w')
+f.write(git.text)
+f.close()
+
+print('Source code:\n' + git.text)
